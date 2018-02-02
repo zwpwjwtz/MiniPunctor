695e4f4c7a26ad4e968799210f01de7ec4ab47e4
zwpwjwtz <zwpwjwtz@126.com>
2018-02-02 22:15:20 +0100

    Add support for MPRIS by talking with DBus

---
+++ mediacontrol.cpp
@@ -0,0 +1,108 @@
+#include "mediacontrol.h"
+
+#include <QDBusInterface>
+#include <QDBusReply>
+
+#define PUNCTOR_DBUS_SERVICE_NAME "org.freedesktop.DBus"
+#define PUNCTOR_DBUS_SERVICE_PATH "/"
+#define PUNCTOR_DBUS_INTERFACE "org.freedesktop.DBus"
+#define PUNCTOR_DBUS_METHOD_LISTNAMES "ListNames"
+
+#define PUNCTOR_MPRIS_SERVICE_NAME "org.mpris.MediaPlayer2"
+#define PUNCTOR_MPRIS_OBJECT_PATH "org/mpris/MediaPlayer2"
+#define PUNCTOR_MPRIS_INTERFACE "org.mpris.MediaPlayer2"
+#define PUNCTOR_MPRIS_PROP_IDENTITY "Identity"
+
+#define PUNCTOR_MPRIS_PLAYER_INTERFACE "org.mpris.MediaPlayer2.Player"
+#define PUNCTOR_MPRIS_PLAYER_PROP_STATUS "PlaybackStatus"
+#define PUNCTOR_MPRIS_PLAYER_PROP_POS "Position"
+#define PUNCTOR_MPRIS_PLAYER_METHOD_PLAY "Play"
+#define PUNCTOR_MPRIS_PLAYER_METHOD_PAUSE "Play"
+#define PUNCTOR_MPRIS_PLAYER_METHOD_STOP "Play"
+#define PUNCTOR_MPRIS_PLAYER_METHOD_SEEK "Play"
+
+
+MediaControl::MediaControl()
+{
+}
+
+int MediaControl::getPlayerList(QList<QString>& playerName,
+                            QList<QString>& playerID)
+{
+    int i;
+    QDBusInterface iface(PUNCTOR_DBUS_SERVICE_NAME,
+                         PUNCTOR_DBUS_SERVICE_PATH,
+                         PUNCTOR_DBUS_INTERFACE);
+    QDBusReply<QList<QString>> reply =
+                                    iface.call(PUNCTOR_DBUS_METHOD_LISTNAMES);
+    for (i=0; i<reply.value().count(); i++)
+    {
+        if (reply.value()[i].indexOf(PUNCTOR_MPRIS_SERVICE_NAME) >= 0)
+            playerID.append(reply.value()[i]);
+    }
+
+    QDBusInterface* iface2;
+    for (i=0; i<playerID.count(); i++)
+    {
+        iface2 = new QDBusInterface(playerID[i],
+                                    PUNCTOR_MPRIS_OBJECT_PATH,
+                                    PUNCTOR_MPRIS_INTERFACE);
+        playerName.append(iface.property(PUNCTOR_MPRIS_PROP_IDENTITY)
+                               .toString());
+        delete iface2;
+    }
+
+    return playerName.count();
+}
+
+MediaControl::MediaStatus MediaControl::getStatus(QString playerID)
+{
+    QDBusInterface iface(playerID,
+                         PUNCTOR_MPRIS_OBJECT_PATH,
+                         PUNCTOR_MPRIS_PLAYER_INTERFACE);
+    QString status = iface.property(PUNCTOR_MPRIS_PLAYER_PROP_STATUS)
+                          .toString();
+    if (status == "Stopped")
+        return status_stopped;
+    return status_unknown;
+}
+
+void MediaControl::play(QString playerID)
+{
+    QDBusInterface iface(playerID,
+                         PUNCTOR_MPRIS_OBJECT_PATH,
+                         PUNCTOR_MPRIS_PLAYER_INTERFACE);
+    iface.call(PUNCTOR_MPRIS_PLAYER_METHOD_PLAY);
+}
+
+void MediaControl::pause(QString playerID)
+{
+    QDBusInterface iface(playerID,
+                         PUNCTOR_MPRIS_OBJECT_PATH,
+                         PUNCTOR_MPRIS_PLAYER_INTERFACE);
+    iface.call(PUNCTOR_MPRIS_PLAYER_METHOD_PAUSE);
+}
+
+void MediaControl::stop(QString playerID)
+{
+    QDBusInterface iface(playerID,
+                         PUNCTOR_MPRIS_OBJECT_PATH,
+                         PUNCTOR_MPRIS_PLAYER_INTERFACE);
+    iface.call(PUNCTOR_MPRIS_PLAYER_METHOD_STOP);
+}
+
+void MediaControl::seek(QString playerID, qint64 pos)
+{
+    QDBusInterface iface(playerID,
+                         PUNCTOR_MPRIS_OBJECT_PATH,
+                         PUNCTOR_MPRIS_PLAYER_INTERFACE);
+    iface.call(PUNCTOR_MPRIS_PLAYER_METHOD_PLAY, pos);
+}
+
+qint64 MediaControl::getPosition(QString playerID)
+{
+    QDBusInterface iface(playerID,
+                         PUNCTOR_MPRIS_OBJECT_PATH,
+                         PUNCTOR_MPRIS_PLAYER_INTERFACE);
+    return iface.property(PUNCTOR_MPRIS_PLAYER_PROP_POS).toLongLong();
+}
---
+++ mediacontrol.h
@@ -0,0 +1,31 @@
+#ifndef MEDIACONTROL_H
+#define MEDIACONTROL_H
+
+#include <QObject>
+#include <QList>
+
+class MediaControl : public QObject
+{
+    Q_OBJECT
+public:
+    enum MediaStatus
+    {
+        status_ready = 0,
+        status_playing = 1,
+        status_paused = 2,
+        status_stopped = 3,
+        status_unknown = 255,
+    };
+
+    MediaControl();
+
+    int getPlayerList(QList<QString>& playerName, QList<QString>& playerID);
+    MediaStatus getStatus(QString playerID);
+    void play(QString playerID);
+    void pause(QString playerID);
+    void stop(QString playerID);
+    void seek(QString playerID, qint64 pos);
+    qint64 getPosition(QString playerID);
+};
+
+#endif // MEDIACONTROL_H
